4928de3fbdbee40776ba3b841a7be87897af926c
swanie21 <kswanie21@gmail.com>
2018-02-03 13:56:52 -0700

    add hot reloading by disabling extract text plugin in dev

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -252,7 +252,7 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new ExtractTextPlugin({ filename: 'styles.css', allChunks: true }),
+    new ExtractTextPlugin({ filename: 'styles.css', allChunks: true, disable: process.env.NODE_ENV !== 'production' }),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
